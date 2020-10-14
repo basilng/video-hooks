7de2e99aff9d12cc0675b1a2e7afef9e01eab762
Basil N G <basilng.bng@gmail.com>
2020-10-14 16:08:43 +0530

    Refactor from class to Functional

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,35 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-  state = {
-    term: "",
+const SearchBar = ({ onSubmit }) => {
+  const [term, setTerm] = useState("");
+
+  const onInputChange = (event) => {
+    setTerm(event.target.value);
   };
 
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value });
-  };
-
-  onFormSubmit = (event) => {
+  const onFormSubmit = (event) => {
     event.preventDefault();
-    this.props.onSubmit(this.state.term);
+    onSubmit(term);
   };
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onFormSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input type="text" value={term} onChange={onInputChange} />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
