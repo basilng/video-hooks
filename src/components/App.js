a030c94fb24da81c4cb54f6a8a50dce17a904976
Basil N G <basilng.bng@gmail.com>
2020-10-14 23:43:14 +0530

    Using the custom Hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
-import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
+  const [videos, search] = useVideos("buildings");
+
   useEffect(() => {
-    onFormSubmit("car");
-  }, []);
-
-  const onFormSubmit = async (term) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    });
-
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
   return (
     <div className="ui container">
-      <SearchBar onSubmit={onFormSubmit} />
+      <SearchBar onSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
